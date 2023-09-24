cf1f9ff77f245f29cd60f5ce4cdfe5b303881347
Raffayet <soviljnikola3@gmail.com>
2023-09-24 11:24:03 +0200

    spring logs update

--- thesis-back/src/main/java/crqlar/thesis/repository/LogRepository.java
+++ thesis-back/src/main/java/crqlar/thesis/repository/LogRepository.java
@@ -2,6 +2,8 @@ package crqlar.thesis.repository;
 
 import crqlar.thesis.model.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LogRepository extends JpaRepository<Log, Long> {
 }
--- thesis-back/src/main/java/crqlar/thesis/repository/UserRepository.java
+++ thesis-back/src/main/java/crqlar/thesis/repository/UserRepository.java
@@ -2,6 +2,8 @@ package crqlar.thesis.repository;
 
 import crqlar.thesis.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
